00196eda2889ef0de8d3bd55221a8175e6ff7aa2
דניאל שלם <dshctr1993@gmail.com>
2023-06-06 13:58:46 +0300

    first commit

--- app/components/Footer.tsx
+++ app/components/Footer.tsx
@@ -2,8 +2,8 @@ const Footer = () => {
   return (
     <footer className="bg-gray-200 text-gray-900 py-4 text-center ">
       <div className="container">
-        <p className="text-sm uppercase">כל הזכויות שמורות©</p>
-        <p>בעלולי בע"מ</p>
+        <p className="text-sm uppercase">{"כל הזכויות שמורות©"}</p>
+        <p>{"בעלולי בעמ"}</p>
       </div>
     </footer>
   );
